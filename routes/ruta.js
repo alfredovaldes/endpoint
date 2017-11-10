f4e0e87935ba4564ea45c406fbbca69232a606ed
alfredo-PC\alfredo <alfredovaldes@uadec.edu.mx>
2017-11-10 00:36:30 -0600

    ruta added

--- routes/ruta.js
+++ routes/ruta.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db.js')
 
+/* User Home. */
+router.get('/', function (req, res, next) {
+  var resultado = 0;
+  var con = db.conexion;
+  con.query("SELECT * FROM user", function (err, result, fields) {
+    if (err) throw err;
+    var data = JSON.stringify(result);  // <====    
+    res.send(data);
+  });
+});
 
-router.route('/:id')
-.get(function(req, res, next){res.send('Res get')})
-.put(function(req, res, next){res.send('Res put')})
-
+/* Other user*/
+/*
+router.get('/:id', function(req, res, next){
+  res.send('Res get')
+});
+*/
 module.exports = router;
