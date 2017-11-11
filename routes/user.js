699449e27119f8fa5626dc17082de18b42b1cbc9
alfredo-PC\alfredo <alfredovaldes@uadec.edu.mx>
2017-11-10 18:04:28 -0600

    paquetes

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var {user} = require('../models')
+var { user } = require('../models')
 
 /* User Home. */
 router.get('/', async function (req, res, next) {
   var resultado = 0;
-  try{
+  try {
     const activeUsers = await user.findAll({})
     const data = JSON.stringify(activeUsers)
     res.render('usersQuery', {
       data: data
     })
-  }catch(err){
-    res.status(500).send({error: 'An error has ocurred'})
+  } catch (err) {
+    res.status(500).send({ error: 'An error has ocurred' })
   }
 });
 
