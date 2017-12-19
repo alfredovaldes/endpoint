a407086c05e8041e6e3da03a0879b0330a632e9f
alfredo-PC\alfredo <alfredovaldes@uadec.edu.mx>
2017-12-19 10:38:15 -0600

    add posicion global

--- controllers/posicioncontroller.js
+++ controllers/posicioncontroller.js
@@ -14,5 +14,29 @@ module.exports = {
         } catch (err) {
             res.status(500).send(err)
         }
+    },
+    async get(req,res){
+        try{
+        var pos;
+        rp('https://testfire-miag5.firebaseio.com/camiones/.json')
+            .then(await function (body) {
+                var respuesta={};
+                var cuerpo = JSON.parse(body);
+                cuerpo = Object.values(cuerpo); 
+                               
+                for(var i=0; i< cuerpo.length;i++){
+                    console.log(cuerpo[i])
+                    if(cuerpo[i]==null){}
+                    else{
+                        var nodo=cuerpo[i]
+                        nodo = Object.values(nodo);
+                         respuesta[i-1]=nodo[nodo.length-1]
+                    }
+                }
+                res.send(respuesta);
+            });
+    } catch (err) {
+        res.status(500).send(err)
+    }
     }
 }
--- routes/posicion.js
+++ routes/posicion.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const posicioncontroller = require('../controllers/posicioncontroller')
-
+router.route('/')
+  .get(posicioncontroller.get)
+  
 router.route('/:id')
   .get(posicioncontroller.show)
 
